723559dca2fd5b0a5fa2425cfeaa3b20c9990116
Angelika Mallek <angelika.mallek03@gmail.com>
2023-02-16 18:20:23 +0100

    added unit in JS

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,26 @@
-let formCurrency = document.querySelector(".js-currency");
-let formQuantity = document.querySelector(".js-quantity");
-let form = document.querySelector(".js-form");
-let result = document.querySelector(".js-result");
+{
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formCurrency = document.querySelector(".js-currency");
+        const formQuantity = document.querySelector(".js-quantity");
+        const result = document.querySelector(".js-result");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+        const formCurrencyValue = formCurrency.value;
+        const amount = +formQuantity.value;
+        let rate;
 
-    let formCurrencyValue = formCurrency.value;
-    let amount = +formQuantity.value;
-    let rate;
-
-    switch (formCurrencyValue) {
-        case "EUR":
-            rate = 4.69;
-            break;
-        case "USD":
-            rate = 4.30;
-            break;
-        case "GBP":
-            rate = 5.25;
-            break;
-    }
-    result.innerText = `Otrzymasz: ${(amount / rate).toFixed(2)} ${formCurrency.value}`;
-})
+        switch (formCurrencyValue) {
+            case "EUR":
+                rate = 4.69;
+                break;
+            case "USD":
+                rate = 4.30;
+                break;
+            case "GBP":
+                rate = 5.25;
+                break;
+        }
+        result.innerText = `Otrzymasz: ${(amount / rate).toFixed(2)} ${formCurrency.value}`;
+    })
+}
